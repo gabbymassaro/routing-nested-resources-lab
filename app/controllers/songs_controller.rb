db4de85aeca6b2b4782bd289c8daba07a7b5e478
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-24 22:17:41 -0400

    All tests pass

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,26 @@
 class SongsController < ApplicationController
+
   def index
-    if params[:artist_name]
-      @songs = Artist.find(params[:artist_name]).songs
-    else
+    if params[:artist_id].nil?
       @songs = Song.all
+    else
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist
+        @songs = artist.songs
+      else
+        flash[:alert] = "Artist not found"
+        redirect_to artists_path
+      end
     end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = Song.find_by(id: params[:id])
+
+    if @song.nil?
+      flash[:alert] = "Song not found."
+      redirect_to artist_songs_path(params[:artist_id])
+    end
   end
 
   def new
